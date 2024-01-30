14072f29075815b1ce9f5ffe553ef00dc65f6fac
DefectingCat <i@defect.ink>
2024-01-30 08:55:18 +0800

    add dotenv suppport

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module rua.plus/gymo
 
 go 1.21.6
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	log.Println("hello world")
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("env file not found")
+	}
 }
