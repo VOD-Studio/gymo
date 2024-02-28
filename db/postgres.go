27365b59d6385a5adcc00a59c115b25ca73c59d8
DefectingCat <i@defect.ink>
2024-02-28 15:14:41 +0800

    添加用户查询测试

--- db/postgres.go
+++ db/postgres.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 
@@ -45,4 +46,21 @@ func MigrateDb() {
 		log.Println(err)
 		return
 	}
+}
+
+func NewMockDB() sqlmock.Sqlmock {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		log.Fatalf("An error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	Db, err = gorm.Open(postgres.New(postgres.Config{
+		Conn: db,
+	}), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalf("An error '%s' was not expected when opening gorm database", err)
+	}
+
+	return mock
 }
--- server/router_test.go
+++ server/router_test.go
@@ -6,9 +6,11 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
 
 	"rua.plus/gymo/controllers"
+	"rua.plus/gymo/db"
 )
 
 func TestRoot(t *testing.T) {
@@ -25,4 +27,37 @@ func TestRoot(t *testing.T) {
 	}
 	resp, _ := json.Marshal(status)
 	assert.Equal(t, resp, w.Body.Bytes())
+}
+
+func TestGetUser(t *testing.T) {
+	mock := db.NewMockDB()
+
+	router := InitRouter()
+
+	var w *httptest.ResponseRecorder
+	var req *http.Request
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", "/v1/user/", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 400, w.Code)
+
+	var rows *sqlmock.Rows
+	rows = sqlmock.NewRows([]string{"id", "username", "email"}).
+		AddRow(1, "xfy", "i@rua.plus")
+	mock.ExpectQuery("^*$").WillReturnRows(rows)
+
+	// user found
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", "/v1/user/?email=i@rua.plus", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+
+	rows = sqlmock.NewRows([]string{"id", "username", "email"})
+	mock.ExpectQuery("^*$").WillReturnRows(rows)
+
+	// user not found
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", "/v1/user/?email=i@rua.plus", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 204, w.Code)
 }
